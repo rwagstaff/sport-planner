35e4dbeb12d432ca7f8acc9d76215af03973bb16
Wagstaff <RWagstaff@eu.ad.espeed.com>
2016-09-20 17:43:24 +0100

    Added date to event repo

--- api/src/main/java/rw/event/Event.scala
+++ api/src/main/java/rw/event/Event.scala
@@ -1,18 +1,21 @@
 package rw.event
 
+import java.time.LocalDateTime
 import javax.persistence._
 
 import rw.entity.{Contact, SequenceId}
 
 @Entity
 @Table
-class Event(n: String) extends SequenceId {
+class Event(n: String, t: LocalDateTime) extends SequenceId {
 
   var name: String = n
 
+  var time: LocalDateTime = t
+
   // For JPA
   def this() {
-    this("")
+    this("", null)
   }
 
 
--- api/src/test/java/rw/event/EventRepositoryTest.scala
+++ api/src/test/java/rw/event/EventRepositoryTest.scala
@@ -1,19 +1,16 @@
 package rw.event
 
-import org.junit.runner.RunWith
-import org.junit.{Ignore, Test}
+import java.time.LocalDateTime
+
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import rw.db.AbstractRepositoryTest
-import rw.friend.FriendRepository
 
 class EventRepositoryTest extends AbstractRepositoryTest[Event] {
   @Autowired val repo: EventRepository = null
 
   override protected def instance: Event = {
-    new Event("new event")
+    new Event("new event", LocalDateTime.now())
   }
 
   override protected def getRepository: JpaRepository[Event, Integer] = {
